b354eacc10d54354fa0050c24a64412c1ee71be6
gimbeomchang <125643426+gimbeomchang@users.noreply.github.com>
2025-01-16 18:23:16 +0900

    [level 1] Title: 실패율, Time: 7.85 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,14 +1,14 @@
 function solution(N, stages) {
-    const processedStages = new Array(N + 1).fill(0);
-    stages.forEach((stage) => processedStages[stage - 1]++);
-    
-    // [index, ratio] 를 원소로 가지는 2차원 배열
-    const fRatio = processedStages.map((s, i) => {
-        const sum = processedStages.slice(i).reduce((acc, cur) => acc + cur, 0);
-        return [i + 1, s / sum];
-    });
-    
-    fRatio.pop(); // 마지막 스테이지를 클리어한 사람의 실패율을 제거함.
-    
-    return fRatio.sort((a, b) => b[1] - a[1]).map((v) => v[0]);
+  const processedStages = new Array(N + 1).fill(0);
+  stages.forEach((stage) => processedStages[stage - 1]++);
+
+  // [index, ratio] 를 원소로 가지는 2차원 배열
+  const fRatio = processedStages.map((s, i) => {
+    const sum = processedStages.slice(i).reduce((acc, cur) => acc + cur, 0);
+    return [i + 1, s / sum];
+  });
+
+  fRatio.pop(); // 마지막 스테이지를 클리어한 사람의 실패율을 제거함.
+
+  return fRatio.sort((a, b) => b[1] - a[1]).map((v) => v[0]);
 }
