89b864fd9c77a2571f5fbbdb92165a4da97cf713
gimbeomchang <125643426+gimbeomchang@users.noreply.github.com>
2025-01-18 16:35:54 +0900

    [level 2] Title: 방문 길이, Time: 0.94 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,32 @@
+const isValidCoordinate = ({x, y}) => {
+    return -5 <= x && x <= 5 && -5 <= y && y <= 5;
+}
+
+const CMD = {
+    U: { x: 0, y: 1 },
+    D: { x: 0, y: -1 },
+    R: { x: 1, y: 0 },
+    L: { x: -1, y: 0 }
+};
+
+const solution = (dirs) => {
+    const visited = new Set();
+    const current = { x: 0, y: 0 };
+    
+    [...dirs].forEach((dir) => {
+        const cmd = CMD[dir];
+        const next = {
+            x: current.x + cmd.x,
+            y: current.y + cmd.y
+        };
+        
+        if (isValidCoordinate(next)) {
+            visited.add(`(${current.x}, ${current.y}) -> (${next.x}, ${next.y})`);
+            visited.add(`(${next.x}, ${next.y}) -> (${current.x}, ${current.y})`);
+            current.x = next.x;
+            current.y = next.y;
+        }
+    })
+    
+    return [...visited].length / 2;
+}
