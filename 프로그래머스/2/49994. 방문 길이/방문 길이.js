11f64e4e7892b14dc7baaaba9db262b262a223fb
gimbeomchang <125643426+gimbeomchang@users.noreply.github.com>
2025-01-18 16:47:45 +0900

    [level 2] Title: 방문 길이, Time: 1.57 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,31 +1,31 @@
-const isValidCoordinate = ({x, y}) => {
-    return -5 <= x && x <= 5 && -5 <= y && y <= 5;
-}
+const isValidCoordinate = ({ x, y }) => {
+  return -5 <= x && x <= 5 && -5 <= y && y <= 5;
+};
 
 const CMD = {
-    U: { x: 0, y: 1 },
-    D: { x: 0, y: -1 },
-    R: { x: 1, y: 0 },
-    L: { x: -1, y: 0 }
+  U: { x: 0, y: 1 },
+  D: { x: 0, y: -1 },
+  R: { x: 1, y: 0 },
+  L: { x: -1, y: 0 },
 };
 
 const solution = (dirs) => {
-    const visited = new Set();
-    const current = { x: 0, y: 0 };
-    
-    [...dirs].forEach((dir) => {
-        const cmd = CMD[dir];
-        const next = {
-            x: current.x + cmd.x,
-            y: current.y + cmd.y
-        };
-        
-        if (isValidCoordinate(next)) {
-            visited.add(`(${current.x}, ${current.y}) -> (${next.x}, ${next.y})`);
-            visited.add(`(${next.x}, ${next.y}) -> (${current.x}, ${current.y})`);
-            Object.assign(current, next);
-        }
-    })
-    
-    return visited.size / 2;
-}
+  const visited = new Set();
+  const current = { x: 0, y: 0 };
+
+  [...dirs].forEach((dir) => {
+    const cmd = CMD[dir];
+    const next = {
+      x: current.x + cmd.x,
+      y: current.y + cmd.y,
+    };
+
+    if (isValidCoordinate(next)) {
+      visited.add(`(${current.x}, ${current.y}) -> (${next.x}, ${next.y})`);
+      visited.add(`(${next.x}, ${next.y}) -> (${current.x}, ${current.y})`);
+      Object.assign(current, next);
+    }
+  });
+
+  return visited.size / 2;
+};
