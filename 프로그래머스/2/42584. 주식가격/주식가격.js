41c5115db57f0b0a513935f3ee24ebadc9b81bdd
gimbeomchang <125643426+gimbeomchang@users.noreply.github.com>
2025-01-22 12:23:58 +0900

    [level 2] Title: 주식가격, Time: 30.92 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,11 +1,19 @@
 const solution = (prices) => {
-  const result = new Array(prices.length).fill(0);
-  for (let i = 0; i < prices.length; i++) {
-    const price = prices[i];
-    for (let j = i + 1; j < prices.length; j++) {
-      result[i]++;
-      if (price > prices[j]) break;
+  const len = prices.length;
+  const result = new Array(len).fill(0);
+  const stack = [0]; // prices 의 index 를 저장하는 stack.
+
+  for (let i = 1; i < len; i++) {
+    while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
+      const j = stack.pop();
+      result[j] = i - j;
     }
+    stack.push(i);
+  }
+
+  while (stack.length > 0) {
+    const j = stack.pop();
+    result[j] = len - j - 1;
   }
 
   return result;
