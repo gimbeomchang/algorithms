65fb62c34e1afa1925b1c41c8da628f3d4676e59
gimbeomchang <125643426+gimbeomchang@users.noreply.github.com>
2025-01-22 12:51:45 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.45 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061#) 
 
 ### 성능 요약
 
-메모리: 36.1 MB, 시간: 17.58 ms
+메모리: 36.3 MB, 시간: 1.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 22일 12:51:00
+2025년 01월 22일 12:51:44
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,25 +1,25 @@
 const solution = (board, moves) => {
-    const n = board.length;
-    let cnt = 0;
-    const bucket = [];
-    
-    moves.forEach((move) => {
-        for (let i = 0; i < n; i++) {
-            const top = bucket[bucket.length - 1];
-            const target = board[i][move - 1];
-            if (target) {
-                if (target === top) {
-                    bucket.pop();
-                    cnt++;
-                } else {
-                    bucket.push(board[i][move - 1]);
-                }
-                
-                board[i][move - 1] = 0;
-                break;
-            }
+  const n = board.length;
+  let cnt = 0;
+  const bucket = [];
+
+  moves.forEach((move) => {
+    for (let i = 0; i < n; i++) {
+      const top = bucket[bucket.length - 1];
+      const target = board[i][move - 1];
+      if (target) {
+        if (target === top) {
+          bucket.pop();
+          cnt++;
+        } else {
+          bucket.push(board[i][move - 1]);
         }
-    });
-    
-    return cnt * 2;
-}
+
+        board[i][move - 1] = 0;
+        break;
+      }
+    }
+  });
+
+  return cnt * 2;
+};
